8bb6f60a72c22bf976432596f16b8ffb7222c847
golu244566 <113621273+golu244566@users.noreply.github.com>
2022-11-05 12:34:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def hello_world():
    return render_template('index.html',user_image=pic1)
 @app.route('/sitemap.xml')
 def sitemap():
-   sitemap_template = render_template('sitemap.xml')
-   response = make_response(sitemap_template)
+   sitemap_xml = render_template(url_for('sitemap'))
+   response = make_response(sitemap_xml)
    response.headers['Content-Type'] = 'application/xml'
    return response
 
